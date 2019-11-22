d3ba681d6b5a43bf6258340eea18cb506a496f6c
W. Trevor King <wking@tremily.us>
2019-11-22 12:40:45 -0800

    prow-success: Add filtering options
    To make it easier to account for all errors affecting a particular job
    pattern.  For example:
    
      $ prow-success.py -v -j 'release.*4.3' -b 'error 1|error 2|error 3'

--- prow-success/prow-success.py
+++ prow-success/prow-success.py
@@ -1,13 +1,68 @@
 #!/usr/bin/env python3
 
+import argparse
 import collections
+import datetime
 import json
+import re
+import urllib.parse
 import urllib.request
 
+
+parser = argparse.ArgumentParser(description='Summarize Prow job success by job name.')
+parser.add_argument(
+    '-v', '--verbose', action='store_const', const=True, default=False,
+    help='Verbose output (e.g. print URIs for failing jobs).')
+parser.add_argument(
+    '-j', '--include-job',
+    help='Only include jobs whose name matches the provided regular expression.')
+parser.add_argument(
+    '-s', '--since',
+    help='Filter to jobs with startTime greater than or equal to this value (YYYY-MM-DDTHH:MM:SSZ).')
+parser.add_argument(
+    '-b', '--exclude-build-log',
+    help='Exclude failures whose build log matches the provided regular expression.')
+
+args = parser.parse_args()
 
 with urllib.request.urlopen('https://prow.svc.ci.openshift.org/prowjobs.js') as response:
     data = response.read()
 jobs = json.loads(data.decode('utf-8'))
+
+if args.include_job:
+    regex = re.compile(args.include_job)
+    jobs['items'] = [
+        job for job in jobs['items']
+        if regex.match(job['spec']['job'])
+    ]
+
+
+if args.since:
+    since = datetime.datetime.strptime(args.since, '%Y-%m-%dT%H:%M:%SZ')
+    jobs['items'] = [
+        job for job in jobs['items']
+        if datetime.datetime.strptime(job.get('status', {}).get('startTime') or '1900-01-01T00:00:00Z', '%Y-%m-%dT%H:%M:%SZ') >= since
+    ]
+
+excluded = set()
+if args.exclude_build_log:
+    params = urllib.parse.urlencode({
+        'name': '.',
+        'maxAge': '24h',
+        'context': '0',
+        'type': 'build-log',
+        'search': args.exclude_build_log,
+    })
+    uri = 'https://search.svc.ci.openshift.org/search?{}'.format(params)
+    with urllib.request.urlopen(uri) as response:
+        data = response.read()
+    excluded = json.loads(data.decode('utf-8'))
+    #print('{}\n  {}'.format(uri, '\n  '.join(excluded.keys())))
+    jobs['items'] = [
+        job for job in jobs['items']
+        if job.get('status', {}).get('url') not in excluded
+    ]
+
 state_counts = collections.defaultdict(lambda: collections.defaultdict(lambda: 0))
 for job in jobs['items']:
     try:
@@ -32,4 +87,16 @@ for job, states in state_counts.items():
     }
 
 for job, rate in sorted(rates.items(), key=lambda job_rate: (-job_rate[1]['rate'], job_rate[1]['done'])):
-    print('{rate:.2f} ({success}/{done}) {job}'.format(job=job, **rate))
+    suffix = ''
+    if args.verbose:
+        uris = [
+            j.get('status', {}).get('url')
+            for j in jobs['items']
+            if j['spec']['job'] == job and j.get('status', {}).get('state') == 'failure'
+        ]
+        uris = [uri for uri in uris if uri]  # remove empty strings
+        if len(uris) > 2:
+            uris = uris[:2] + ['...']
+        if uris:
+            suffix = ' {}'.format(' '.join(uris))
+    print('{rate:.2f} ({success}/{done}) {job}{suffix}'.format(job=job, suffix=suffix, **rate))
